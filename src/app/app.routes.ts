e25bdeff3e2815556e05b33ed5c2ad397d56ccc1
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-05-14 19:11:00 +0200

    Layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from '@products/pages/list/list.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ListComponent
+      },
+    ]
   },
   {
     path: '**',
     component: NotFoundComponent
-  }
+  },
 ];
